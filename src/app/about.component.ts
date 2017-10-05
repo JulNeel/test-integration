d6cff4c3aacba1e54bdb6f55afb957b06ce67818
Jul Neel <julien.bruneel@gmail.com>
2017-10-05 15:23:38 +0200

    enable ie11

--- src/app/about.component.ts
+++ src/app/about.component.ts
@@ -2,7 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'about',
-  template: `<h1>About Page</h1>`
+  template: `<div class="container">
+  				<h1 class="text-center mb-5">About Page</h1>
+  				<p>Proin iaculis purus consequat sem cure digni. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. Proin iaculis purus consequat sem cure digni. Donec porttitora entum suscipit aenean rhoncus posuere odio in tincidunt. 
+  				</p>
+  			</div>`
 })
 export class AboutComponent {
   title = 'About';
